bf71c38737eeaa60dae77bd1cf34f22527cdc2b7
bohdan zhylavskyi <zhylavskyibohdan@gmail.com>
2025-07-27 17:53:05 +0300

    add trigger

---
+++ SQLProject/SQLProject/Scripts/Script.PostDeployment.InsertEmployeeTrigger.sql
@@ -0,0 +1,13 @@
+﻿CREATE TRIGGER InsertEmployeeTrigger
+ON Employees
+AFTER INSERT
+AS
+BEGIN
+    SET NOCOUNT ON;
+
+    INSERT INTO Companies (Name, AddressId)
+    SELECT 
+        i.CompanyName,
+        i.AddressId
+    FROM inserted i;
+END
--- SQLProject/SQLProject/Scripts/Script.PostDeployment.sql
+++ SQLProject/SQLProject/Scripts/Script.PostDeployment.sql
@@ -10,44 +10,8 @@ Post-Deployment Script Template
 --------------------------------------------------------------------------------------
 */
 
-PRINT 'Starting post-deployment script...'
+:r .\Script.PostDeployment.Seed.sql
 
-INSERT INTO Persons (FirstName, LastName)
-SELECT 'Bohdan', 'Zhylavskyi'
-WHERE NOT EXISTS (
-    SELECT 1
-    FROM Persons
-    WHERE FirstName = 'Bohdan' AND LastName = 'Zhylavskyi'
-);
+GO
 
-INSERT INTO Addresses (Street, City, State, ZipCode)
-SELECT v.Street, v.City, v.State, v.ZipCode
-FROM (VALUES
-    ('123 Maple St', 'Springfield', 'IL', '62704'),
-    ('456 Oak Ave', 'Madison',     'WI', '53703'),
-    ('789 Pine Rd', 'Boulder',     'CO', '80301')
-) AS v (Street, City, State, ZipCode)
-WHERE NOT EXISTS (
-    SELECT 1 FROM Addresses a WHERE a.ZipCode = v.ZipCode
-);
-
-INSERT INTO Employees (AddressId, PersonId, CompanyName, Position, EmployeeName)
-SELECT (SELECT TOP 1 Id FROM Addresses WHERE ZipCode = '62704'),
-(SELECT TOP 1 Id FROM Persons WHERE FirstName = 'Bohdan' AND LastName = 'Zhylavskyi'),
-'EPAM',
-'Software Engineer',
-'Bohdan Zhylavskyi'
-WHERE NOT EXISTS (
-    SELECT 1
-    FROM Employees
-    WHERE PersonId = (SELECT TOP 1 Id FROM Persons WHERE FirstName = 'Bohdan' AND LastName = 'Zhylavskyi')
-)
-
-INSERT INTO Companies (Name, AddressId)
-SELECT 'EPAM',
-(SELECT TOP 1 Id FROM Addresses WHERE ZipCode = '53703')
-WHERE NOT EXISTS (
-    SELECT 1
-    FROM Companies
-    WHERE Name = 'EPAM'
-)
+:r .\Script.PostDeployment.InsertEmployeeTrigger.sql
